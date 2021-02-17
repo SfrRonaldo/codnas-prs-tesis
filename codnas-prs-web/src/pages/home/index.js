6a4255b4c5434c7bc5619b4160c8bc9f3377e533
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-02-17 12:58:01 -0500

    feature: fix button styles

--- codnas-prs-web/src/pages/home/index.js
+++ codnas-prs-web/src/pages/home/index.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Parallax } from "react-parallax";
 import Search from "../../components/search";
 import "./index.css";
 
 const Home = () => {
   return (
-    <div className="container">
+    <Parallax bgImage="/images/img-home.png" bgImageAlt="prs">
       <Search
         title="Herramienta para el análisis de diversidad conformacional en estructuras de proteínas repetidas"
         text="Ingresar una cadena de proteína repetida para buscar su información de diversidad conformacional en la base de datos o estimar su diversidad conformacional agregando la región de repetición de la misma"
         color="primary"
       />
-    </div>
+    </Parallax>
   );
 };
 
