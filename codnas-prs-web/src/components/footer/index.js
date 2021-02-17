7fc7a5a13e8f3f9861058753d6e201c4d31ea42c
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2021-02-17 12:22:52 -0500

    feature: fix footer

--- codnas-prs-web/src/components/footer/index.js
+++ codnas-prs-web/src/components/footer/index.js
@@ -1,115 +1,43 @@
 import React from "react";
 import "./index.css";
-import Button from "../button";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <section className="footer-subscription">
-        <p className="footer-subscription-heading">
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className="footer-subscription-text">
-          You can unsubscribe at any time.
-        </p>
-        <div className="input-areas">
-          <form>
-            <input
-              className="footer-input"
-              name="email"
-              type="email"
-              placeholder="Your Email"
-            />
-            <Button buttonStyle="btn--outline">Subscribe</Button>
-          </form>
-        </div>
-      </section>
-      <div class="footer-links">
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
-          </div>
-          <div class="footer-link-items">
-            <h2>Contact Us</h2>
-            <Link to="/">Contact</Link>
-            <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
-            <Link to="/">Sponsorships</Link>
-          </div>
-        </div>
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>Videos</h2>
-            <Link to="/">Submit Video</Link>
-            <Link to="/">Ambassadors</Link>
-            <Link to="/">Agency</Link>
-            <Link to="/">Influencer</Link>
-          </div>
-          <div class="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
-          </div>
-        </div>
-      </div>
-      <section class="social-media">
-        <div class="social-media-wrap">
-          <div class="footer-logo">
-            <Link to="/" className="social-logo">
-              TRVL
-              <i class="fab fa-typo3" />
-            </Link>
-          </div>
-          <small class="website-rights">TRVL © 2020</small>
-          <div class="social-icons">
-            <Link
-              class="social-icon-link facebook"
-              to="/"
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <small className="website-rights">
+            Copyright © 2021. Hecho por Ronaldo Romario Tunque Cahui. Todos los
+            derechos reservados.
+          </small>
+          <div className="social-icons">
+            <a
+              className="social-icon-link facebook"
+              href="https://www.facebook.com/RonaldoTunqueC"
               target="_blank"
+              rel="noreferrer"
               aria-label="Facebook"
             >
-              <i class="fab fa-facebook-f" />
-            </Link>
-            <Link
-              class="social-icon-link instagram"
-              to="/"
+              <i className="fab fa-facebook-f" />
+            </a>
+            <a
+              className="social-icon-link github"
+              href="https://github.com/SfrRonaldo"
               target="_blank"
-              aria-label="Instagram"
+              rel="noreferrer"
+              aria-label="GitHub"
             >
-              <i class="fab fa-instagram" />
-            </Link>
-            <Link
-              class="social-icon-link youtube"
-              to="/"
+              <i className="fab fa-github" />
+            </a>
+            <a
+              className="social-icon-link linkedin"
+              href="https://www.linkedin.com/in/ronaldotunquecahui/"
               target="_blank"
-              aria-label="Youtube"
-            >
-              <i class="fab fa-youtube" />
-            </Link>
-            <Link
-              class="social-icon-link twitter"
-              to="/"
-              target="_blank"
-              aria-label="Twitter"
-            >
-              <i class="fab fa-twitter" />
-            </Link>
-            <Link
-              class="social-icon-link twitter"
-              to="/"
-              target="_blank"
+              rel="noreferrer"
               aria-label="LinkedIn"
             >
-              <i class="fab fa-linkedin" />
-            </Link>
+              <i className="fab fa-linkedin" />
+            </a>
           </div>
         </div>
       </section>
